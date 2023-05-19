8ac64cd669a26a9b30fdc4942d9f402b311372e4
Davide Spinozzi <davidespinozzi01@gmail.com>
2023-05-19 15:38:03 +0200

    .

--- assets/JS/album.js
+++ assets/JS/album.js
@@ -252,10 +252,24 @@ function displayAudioPlayer(data) {
   const playerDiv2 = document.createElement('div');
   playerDiv2.classList.add('col-0', 'col-md-8', 'd-flex', 'flex-column', 'd-none', 'd-md-block');
   const playerButtonDiv = document.createElement('div');
-  playerButtonDiv.classList.add('text-center', 'm-1')
+  playerButtonDiv.classList.add('d-flex', 'm-1', 'justify-content-center')
+  const shuffle = document.createElement('div')
+  shuffle.classList.add('d-flex', 'justify-content-center', 'align-items-center')
+  shuffle.innerHTML +=`<svg  onclick='toggleGreen(this)' xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-shuffle me-3" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M0 3.5A.5.5 0 0 1 .5 3H1c2.202 0 3.827 1.24 4.874 2.418.49.552.865 1.102 1.126 1.532.26-.43.636-.98 1.126-1.532C9.173 4.24 10.798 3 13 3v1c-1.798 0-3.173 1.01-4.126 2.082A9.624 9.624 0 0 0 7.556 8a9.624 9.624 0 0 0 1.317 1.918C9.828 10.99 11.204 12 13 12v1c-2.202 0-3.827-1.24-4.874-2.418A10.595 10.595 0 0 1 7 9.05c-.26.43-.636.98-1.126 1.532C4.827 11.76 3.202 13 1 13H.5a.5.5 0 0 1 0-1H1c1.798 0 3.173-1.01 4.126-2.082A9.624 9.624 0 0 0 6.444 8a9.624 9.624 0 0 0-1.317-1.918C4.172 5.01 2.796 4 1 4H.5a.5.5 0 0 1-.5-.5z"/>
+  <path d="M13 5.466V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192zm0 9v-3.932a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192z"/>
+</svg>`;
+  
   const playerPlay = document.createElement('img');
   playerPlay.src = "./assets/imgs/play-circle-fill.svg"
   playerPlay.classList.add('playerPlay')
+
+  const loop = document.createElement('div')
+  loop.classList.add('d-flex', 'justify-content-center', 'align-items-center')
+  loop.innerHTML +=`<svg xmlns="http://www.w3.org/2000/svg" onclick='toggleGreen(this)' width="22" height="22" fill="currentColor" class="bi bi-arrow-counterclockwise ms-3" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 3a5 5 0 1 1-4.546 2.914.5.5 0 0 0-.908-.417A6 6 0 1 0 8 2v1z"/>
+  <path d="M8 4.466V.534a.25.25 0 0 0-.41-.192L5.23 2.308a.25.25 0 0 0 0 .384l2.36 1.966A.25.25 0 0 0 8 4.466z"/>
+</svg>`
 
   const staticBar = document.createElement('div');
   staticBar.classList.add('staticBar');
@@ -286,7 +300,9 @@ function displayAudioPlayer(data) {
   div1Buttons.appendChild(hiddenPlayerPlay);
   div1Buttons.appendChild(hiddenPlayerClose);
   playerDiv2.appendChild(playerButtonDiv);
+  playerButtonDiv.appendChild(shuffle);
   playerButtonDiv.appendChild(playerPlay);
+  playerButtonDiv.appendChild(loop);
   playerDiv2.appendChild(staticBar);
   staticBar.appendChild(playerBar);
   playerBar.appendChild(audioTrack);
