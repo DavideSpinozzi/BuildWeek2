3022ba316a47fcfc5fe7f62e3e9b7dfb9f8513c4
Walter Barbieri <128515233+uolli90@users.noreply.github.com>
2023-05-19 16:36:33 +0200

    Update album.js

--- assets/JS/album.js
+++ assets/JS/album.js
@@ -107,6 +107,7 @@ fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${albumID}`)
       <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
       </svg></div> </div>
     <span class="material-symbols-outlined cursor animation p-3 onclick = 'toggleGreen(this) '">
+    <span class="material-symbols-outlined cursor animation p-3" onclick = 'toggleGreen(this)'>
                         favorite
                     </span>
                     <span class="material-symbols-outlined cursor  p-3">
